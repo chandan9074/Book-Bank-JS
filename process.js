d1320de0ee221c6fd93e090536ae730c1107ff0b
chandan9074 <chandan15-11611@diu.edu.bd>
2021-08-18 23:06:31 +0600

    feat: Build cash out and balance logic

--- process.js
+++ process.js
@@ -1,3 +1,5 @@
+
+// cash in button on click part
 document.getElementById("cash-in-btn").addEventListener('click', function(){
     // calculation of cash in part 
     const cashInputAmount = document.getElementById("cash-in-inpt");
@@ -15,4 +17,32 @@ document.getElementById("cash-in-btn").addEventListener('click', function(){
     const parseBalanceSCR = parseFloat(balanceSCR.innerText);
 
     balanceSCR.innerHTML = parseBalanceSCR + parseCashAmount;
+})
+
+// buy button on click part
+
+document.getElementById("buy-btn").addEventListener('click', function(){
+
+    // calcultaion of cash out part with buy part
+    const bookNo = document.getElementById("no-books");
+    const parseBookNo = parseFloat(bookNo.value);
+
+    const bookPrice = document.getElementById("books-price");
+    const parseBookPrice = parseFloat(bookPrice.value);
+
+    const multiplyBoth = parseBookNo * parseBookPrice;
+
+    const cashOutSCR = document.getElementById("cash-out-field");
+    const parseCashOutSCR = parseFloat(cashOutSCR.innerText);
+
+    cashOutSCR.innerHTML = parseCashOutSCR + multiplyBoth;
+
+    // clear both input field
+    bookNo.value = "";
+    bookPrice.value = "";
+
+    // menaging the balance part
+    const totalBalanceSCR = document.getElementById("balance-field");
+    const parsetotalBalanceSCR = parseFloat(totalBalanceSCR.innerText);
+    totalBalanceSCR.innerHTML = parsetotalBalanceSCR -  multiplyBoth;
 })
